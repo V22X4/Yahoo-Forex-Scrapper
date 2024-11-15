57a126b2d847fef4d844008c315e1b56127b4c4d
Vishal Singh Tanwar <vishal22102004@gmail.com>
2024-11-15 16:13:36 +0530

    fix backend

--- backend/app.py
+++ backend/app.py
@@ -8,6 +8,7 @@ import signal
 import sys
 import time
 import logging
+import os
 from werkzeug.serving import is_running_from_reloader
 from contextlib import contextmanager
 from utils import scrape_forex_data, update_forex_data, init_database
@@ -148,7 +149,9 @@ if __name__ == '__main__':
     
     try:
         # Run Flask app
-        app.run(debug=True, use_reloader=True)
+        port = int(os.getenv("PORT", 3000))  # Default to 5000 if not defined
+        app.run(debug=True, use_reloader=True, host='0.0.0.0', port=port)
+
     except Exception as e:
         logger.error(f"Error starting Flask app: {e}")
     finally:
